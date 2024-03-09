48e2b3ca17596fa21e8502c949d4111a65f8f480
Rohit <rohit@MacBook-Air.local>
2024-03-09 18:41:40 +0530

    main: created products page, added layout logic to the home page.

--- store/src/app/pages/home/home.component.ts
+++ store/src/app/pages/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT : {[id: number]: number} = {
+  1: 400,
+  3: 335,
+  4: 350
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,6 +15,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+
+  category: string | undefined;
 
   constructor() { }
 
@@ -17,6 +26,11 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number) : void {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 
 }
